1c125528bbac0486866ba6c278e2b90000d4f942
Vinay <vinayprakash.allsober@gmail.com>
2023-08-15 20:48:22 +0530

    sasa

--- .github/workflows/api-call-node-js.js
+++ .github/workflows/api-call-node-js.js
@@ -5,7 +5,23 @@ const url = 'https://automation.atlassian.com/pro/hooks/7e0c8982c6766ee66128b036
 const data = {
   "issues": ["ISM1-10"],
   "data": {
-    "releaseVersion": `workingfrom piepline`
+    "releaseVersion": `Terraform Format and Style 🖌failure
+
+    Terraform Initialization ⚙️success
+    
+    Check Terraform state file 🔎: \`\`
+    
+    Terraform Validation 🤖$success
+    
+    <details><summary>Validation Output</summary>
+    
+    
+    [32m[1mSuccess![0m The configuration is valid.
+    [0m
+    
+    </details>
+    
+    Pusher: @vinayprakash893, Action: pull_request, Working Directory: /home/runner/work/azure-terrafrom-vny-actions/azure-terrafrom-vny-actions/app2, Workflow: caller-reusable-with-approval-app2`
   }
 };
 
