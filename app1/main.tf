832cd6434961112678232ae51a68823a291516b5
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2023-08-15 20:27:09 +0530

    Update main.tf

--- app1/main.tf
+++ app1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-bfe2059c-playground-sansdds"
+#   name     = "1-bfe2059c-playground-sansddboxs"
 #   location = "southcentralus"
 # }
 
