0b88f7f8898adb6839365200c6e7e968cc130323
Vinay <vinayprakash.allsober@gmail.com>
2023-08-16 15:07:32 +0530

    sasa

--- .github/workflows/api-jira-json.py
+++ .github/workflows/api-jira-json.py
@@ -2,24 +2,7 @@ import json
 import requests
 
 data = {
-"issues":["ISM1-10"], "data": {"commentdata": """
-#### Terraform Format and Style 🖌`failure`
-#### Terraform Initialization ⚙️`success`
-#### Check Terraform state file 🔎:  ``
-#### Terraform Validation 🤖`$success`
-<details><summary>Validation Output</summary>
-
-```
-
-[32m[1mSuccess![0m The configuration is valid.
-[0m
-
-```
-
-</details>
-
-*Pusher: @vinayprakash893, Action: `pull_request`, Working Directory: `/home/runner/work/azure-terrafrom-vny-actions/azure-terrafrom-vny-actions/app2`, Workflow: `caller-reusable-with-approval-app2`*
-"""}}
+"issues":["ISM1-10"], "data": {"commentdata": """${{ github.event.comment.body }}"""}}
 
 
 headers = {
