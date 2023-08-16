f3093784812021eb4451f98d5099b858b9b6cf5a
Vinay <vinayprakash.allsober@gmail.com>
2023-08-16 17:56:30 +0530

    sasa

--- .github/workflows/api-call-node-js.js
+++ .github/workflows/api-call-node-js.js
@@ -1,50 +1,78 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-const url = 'https://automation.atlassian.com/pro/hooks/7e0c8982c6766ee66128b036964ae062592d1a69'; // Replace with the actual target URL
+// const url = 'https://automation.atlassian.com/pro/hooks/7e0c8982c6766ee66128b036964ae062592d1a69'; // Replace with the actual target URL
+
+// const data = {
+//   "issues": ["ISM1-10"],
+//   "data": {
+//     "commentdata": "#### Terraform Format and Style 🖌`failure`
+// #### Terraform Initialization ⚙️`success`
+// #### Check Terraform state file 🔎:  ``
+// #### Terraform Validation 🤖`$success`
+// <details><summary>Validation Output</summary>
+
+// ```
+
+// Success! The configuration is valid.
+
+
+// ```
+
+// </details>
+
+// *Pusher: @vinayprakash893, Action: `pull_request`, Working Directory: `/home/runner/work/azure-terrafrom-vny-actions/azure-terrafrom-vny-actions/app2`, Workflow: `caller-reusable-with-approval-app2`*'''
+//   }
+// };
+
+// // const multilineString = ```
+// //   ###This is a multiline string.`sasa`
+// //   It can span multiple lines without using escape characters.
+// //   You can include variables and expressions inside placeholders.
+// //   Special characters like backticks \` and newlines work as expected.
+// // ```;
+// // console.log(multilineString)
+
+
+
+// data.data.commentdata = data.data.commentdata.replace(/`/g, '"');
+
+// const headers = {
+//   "Content-Type": "application/json"
+// };
+
+// console.log(data)
+
+
+// axios.post(url, JSON.stringify(data), { headers })
+//   .then(response => {
+//     console.log('Response:', response.data);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
 
 const data = {
   "issues": ["ISM1-10"],
   "data": {
-    "commentdata": '"""#'### Terraform Format and Style 🖌`failure`
-#### Terraform Initialization ⚙️`success`
-#### Check Terraform state file 🔎:  ``
-#### Terraform Validation 🤖`$success`
-<details><summary>Validation Output</summary>
-
-```
-
-Success! The configuration is valid.
-
-
-```
-
-</details>
-
-*Pusher: @vinayprakash893, Action: `pull_request`, Working Directory: `/home/runner/work/azure-terrafrom-vny-actions/azure-terrafrom-vny-actions/app2`, Workflow: `caller-reusable-with-approval-app2`*'''
+    "commentdata": "#### Terraform Format and Style 🖌`failure`
+    #### Terraform Initialization ⚙️`success`
+    #### Check Terraform state file 🔎:  ``
+    #### Terraform Validation 🤖`$success`
+    <details><summary>Validation Output</summary>
+    
+    ```
+    
+    Success! The configuration is valid.
+    
+    
+    ```
+    
+    </details>
+    
+    *Pusher: @vinayprakash893, Action: `pull_request`, Working Directory: `/home/runner/work/azure-terrafrom-vny-actions/azure-terrafrom-vny-actions/app2`, Workflow: `caller-reusable-with-approval-app2`*"
   }
 };
 
-// const multilineString = ```
-//   ###This is a multiline string.`sasa`
-//   It can span multiple lines without using escape characters.
-//   You can include variables and expressions inside placeholders.
-//   Special characters like backticks \` and newlines work as expected.
-// ```;
-// console.log(multilineString)
-
-data.data.commentdata = data.data.commentdata.replace(/`/g, '"');
-
-const headers = {
-  "Content-Type": "application/json"
-};
-
-console.log(data)
-
-
-axios.post(url, JSON.stringify(data), { headers })
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+const jsonString = JSON.stringify(data, null, 2);
+console.log(jsonString);
