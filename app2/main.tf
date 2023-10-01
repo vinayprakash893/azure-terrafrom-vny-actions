c19095b1a76b27f3f72cb83b72b1b964ba0f2aaa
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2023-10-01 19:24:50 +0530

    Update main.tf

--- app2/main.tf
+++ app2/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-bfe2059c-playground-sandb444dsdokkdddssz"
+#   name     = "1-bfe2059c-playground-sanddddssz"
 #   location = "southcentralus"
 # }
 
