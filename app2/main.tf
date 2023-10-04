cafc20b3b59f37b11c483b5dadbab261d2b0ae89
Vinay <vinayprakash.allsober@gmail.com>
2023-10-04 22:38:58 +0530

    sas

--- app2/main.tf
+++ app2/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-bfe2059c-playground-sffssaz"
+#   name     = "1-bfe2059c-playground-sffsasssaz"
 #   location = "southcentralus"
 # }
 
