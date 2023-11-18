08858069577d930bfd67cdaa8f33a4033b1ac01a
vinayprakash893 <vinayprakash893@users.noreply.github.com>
2023-11-18 16:39:02 +0000

    [AUTO]Automatic commit
    Files changed:
    A	.github/workflows/caller-reusable-with-approval-testworkflow.yaml
    A	.github/workflows/locals.tf

---
+++ .github/workflows/locals.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+  cloud {
+    organization = "Cloudtech"
+
+    workspaces {
+      name = ""
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = ""
+#   location = ""
+# }
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = ""
+  resource_group_name      = "1-bfe2059c-playground-sandbox"
+  location                 = 
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  newvalue = false
+}
+
+
