d48061d422cfdb6ffb302e68ccd7bd07e5ab59a8
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2023-11-28 18:33:01 +0530

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-cdf==df5-playground-sandbox"
+#   name     = "1-cdfdf5-playground-sandbox"
 #   location = "eastus"
 # }
 
