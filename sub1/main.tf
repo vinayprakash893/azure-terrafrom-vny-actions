47e7916c19cbb0a35f69d0b1f6d6c4887ea4597b
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2023-11-28 16:55:14 +0530

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-cdcssbf205-playground-sandbox"
+#   name     = "1-cdcssb05-playground-sandbox"
 #   location = "eastus"
 # }
 
