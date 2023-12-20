5c5e5d49dcdbfde46614a0073090951df39fa02c
Vinay <vinayprakash.allsober@gmail.com>
2023-12-20 19:33:57 +0530

    sasa

--- actions/utility-master/pre_work.sh
+++ actions/utility-master/pre_work.sh
@@ -4,12 +4,12 @@ echo "myfile"
 echo "input is ${pre_clean}"
 echo "path is ${clean_path}"
 if [ "${pre_clean}" == true ]; then
-  echo "Running post-job cleanup..."
+  echo "Running pre-job cleanup..."
   if [ "${clean_path}" != 'false' ]; then
     rm -rf ${clean_path}
     echo "Cleaned ${clean_path}"
   fi
 
 else
-  echo "Post cleanup is skipped."
+  echo "pre cleanup is skipped."
 fi
