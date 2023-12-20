da79d5ff5775c580a3f2dfccb3f62538881e0292
Vinay <vinayprakash.allsober@gmail.com>
2023-12-20 19:00:43 +0530

    sasa

--- actions/utility-master/post_cleanup.sh
+++ actions/utility-master/post_cleanup.sh
@@ -2,14 +2,15 @@
 echo "input is ${POST_CLEAN}"
 if [ "${POST_CLEAN}" == true ]; then
   echo "Running post-job cleanup..."
-  set -o errexit -o nounset -o xtrace -o pipefail
-  shopt -s inherit_errexit nullglob dotglob
+  rm -rf ${{ github.sha }}
+  # set -o errexit -o nounset -o xtrace -o pipefail
+  # shopt -s inherit_errexit nullglob dotglob
 
-  rm -rf "${HOME:?}"/* "${GITHUB_WORKSPACE:?}"/*
+  # rm -rf "${HOME:?}"/* "${GITHUB_WORKSPACE:?}"/*
 
-  if test "${RUNNER_DEBUG:-0}" != '1'; then
-    set +o xtrace
-  fi
+  # if test "${RUNNER_DEBUG:-0}" != '1'; then
+  #   set +o xtrace
+  # fi
 else
   echo "Post cleanup is skipped."
 fi
