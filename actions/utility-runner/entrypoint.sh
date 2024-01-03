1a87a9979bfaf8c2b27f75e16449fb725a656016
Vinay <vinayprakash.allsober@gmail.com>
2024-01-03 19:57:53 +0530

    sasa

--- actions/utility-runner/entrypoint.sh
+++ actions/utility-runner/entrypoint.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
-echo "Custom-Utility"
-echo "$1"
+if [ -n "${pres_custom_script}" ]; then
+  echo "Executing ${pres_custom_script}"
+  eval "${pres_custom_script}"
+else
+  echo "Pre Task is skipped."
+fi
