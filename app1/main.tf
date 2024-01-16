d3d2e0e84b9c814761152155c28c669ae2bbe89b
Vinay <vinayprakash.allsober@gmail.com>
2024-01-16 22:04:19 +0530

    fdfd

--- app1/main.tf
+++ app1/main.tf
@@ -32,7 +32,7 @@ resource "random_string" "uniquestring" {
 # }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "mystoragevnyacgtest"
+  name                     = "mystoragfdevnyacgtest"
   resource_group_name      = "1-bfe2059c-playground-sandbox"
   location                 = "southcentralus"
   account_tier             = "Standard"
