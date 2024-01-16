c171abf677e4358dd385dc6186627974a5c281cf
Vinay <vinayprakash.allsober@gmail.com>
2024-01-16 21:54:25 +0530

    sasa

--- app1/main.tf
+++ app1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resource_group" "rg" {
-#   name     = "1-bfe2059c-playgrjound-sandbosgggfxs"
+#   name     = "1-bfe2059c-playgrjnd-sandbosgggfxs"
 #   location = "southcentralus"
 # }
 
