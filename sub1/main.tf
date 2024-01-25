0f39f5fe9a7f6c226cf4cdf3b9527c2157825d2d
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2024-01-25 17:39:33 +0530

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -26,7 +26,7 @@ resource "random_string" "uniquestring" {
   upper   = false
 }
 
-# resource "azurerm_resourfce_grodup" "rg" {
+# resource "azurerm_resource_grodup" "rg" {
 #   name     = "1-cdfs77dffodn-sandbox"
 #   location = "eastus"d
 # }
