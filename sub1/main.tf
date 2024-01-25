621f1e25c02ff4140be60a392a26e7848b5b64f0
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2024-01-25 19:27:18 +0530

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -32,7 +32,6 @@ resource "random_string" "uniquestring" {
 # }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name   = "test11vny111mydoamins"
   count                     = 80
   resource_group_name      = "1-cdcbf205-playground-sandbox"
   location                 = "eastus"
