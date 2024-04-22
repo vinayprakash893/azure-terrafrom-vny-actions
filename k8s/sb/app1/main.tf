fa875fffa14ee9712cbcc1f50d3942b42ea722ac
Vinay <vinayprakash.allsober@gmail.com>
2024-04-22 22:47:08 +0530

    s

--- k8s/sb/app1/main.tf
+++ k8s/sb/app1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resofurce_group" "rg" {
-#   name     = "1-bfdfsgnd-sasdssggfxs"
+#   name     = "1-bfdfsgnd-sassggfxs"
 #   location = "southcentralus"
 # }
 
