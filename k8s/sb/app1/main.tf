0ca01d69db1567fa8407ac88eb58c715bfe7caca
Vinay <vinayprakash.allsober@gmail.com>
2024-04-22 22:44:03 +0530

    sasa

--- k8s/sb/app1/main.tf
+++ k8s/sb/app1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resofurce_group" "rg" {
-#   name     = "1-bfdfsgnd-sasdsssgggfxs"
+#   name     = "1-bfdfsgnd-sasdssggfxs"
 #   location = "southcentralus"
 # }
 
--- k8s/sb/app2/main.tf
+++ k8s/sb/app2/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resofurce_group" "rg" {
-#   name     = "1-bfdfsgnd-ssasdbossggfxs"
+#   name     = "1-bfdfsgnd-ssasdboggfxs"
 #   location = "southcentralus"
 # }
 
