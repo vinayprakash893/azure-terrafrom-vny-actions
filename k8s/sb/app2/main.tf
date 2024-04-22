2af4e81ee1b1226752d7dfa0fa5258875e4fe1fe
Vinay <vinayprakash.allsober@gmail.com>
2024-04-22 19:02:55 +0530

    dsds

---
+++ k8s/sb/app2/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+  cloud {
+    organization = "Cloudtech"
+
+    workspaces {
+      name = "cloud_user_p_816fb095"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+# resource "azurerm_resofurce_group" "rg" {
+#   name     = "1-bfdfsgnd-ssasdbosssssgggfxs"
+#   location = "southcentralus"
+# }
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "mystoragfdevnyacgtest"
+  resource_group_name      = "1-bfe2059sac-playground-sandbox"
+  location                 = "southcentralus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
