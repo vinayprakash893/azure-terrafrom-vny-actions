34d843c191fd3772be78dc8cf0bf2dd1bc09fd8c
vinayprakash893 <38611502+vinayprakash893@users.noreply.github.com>
2024-05-21 10:59:39 +0530

    Update main.tf

--- app1/main.tf
+++ app1/main.tf
@@ -27,7 +27,7 @@ resource "random_string" "uniquestring" {
 }
 
 # resource "azurerm_resofurce_group" "rg" {
-#   name     = "1-bfdfsgnsd-ssssagsssfsxs"
+#   name     = "1-bfdfsgnsd-ssssasgsssfsxs"
 #   location = "southcentralus"
 # }
 
