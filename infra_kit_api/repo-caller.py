9ff2f263d4d47d12a30b5669ce7530f91f7db5b6
Vinay <Developer12@virtuoustech.com>
2024-06-24 17:13:16 +0530

    jinjate

--- infra_kit_api/repo-caller.py
+++ infra_kit_api/repo-caller.py
@@ -9,7 +9,8 @@ def read_json_map_file(file_name, env):
             data = json.load(file)
             if env in data:
                 result = data[env]
-                result["Organization_Name"] = data["Organization_Name"]
+                result["TF_Organization_Name"] = data["TF_Organization_Name"]
+                result["Infrakit_Template_Repo"] = data["Infrakit_Template_Repo"]
                 return result
             else:
                 print(f"Environment '{env}' not found in the JSON file.")
@@ -29,8 +30,11 @@ def merge_with_input(input_file, new_data):
         print(f"The file '{input_file}' does not exist. Creating a new one.")
         existing_data = {}
     
+    infra_metadata = {
+        "Infra_Metadata": new_data
+    }
     # Merge new data into existing data
-    existing_data.update(new_data)
+    existing_data.update(infra_metadata)
     
     with open(input_file, 'w') as file:
         json.dump(existing_data, file, indent=2)
@@ -47,10 +51,14 @@ def trigger_github_action(repository, merged_file):
       print("Token retrieved successfully")
     else:
         print("Token not found")
+        print(f"Calling repo : {repository}")
+        api_url = f'https://api.github.com/repos/vinayprakash893/{repository}/dispatches'
+        print(f"Calling repo : {api_url}")
         sys.exit(1)
+    print(f"Calling repo : {repository}")
     api_url = f'https://api.github.com/repos/vinayprakash893/{repository}/dispatches'
     headers = {
-        'Accept': 'application/vnd.github.everest-preview+json',
+        'Accept': 'application/vnd.github+json',
         'Authorization': f'Bearer {token}',
     }
     with open(merged_file, 'r') as file:
@@ -59,6 +67,7 @@ def trigger_github_action(repository, merged_file):
     data = {
         'event_type': 'self_service2',  # Customize this event type as needed
         'client_payload': {
+          
             'workflow_data': merged_data
         },
     }
@@ -82,7 +91,7 @@ if __name__ == "__main__":
     # file_prefix =  os.getenv('jsontemp_Platform')
     # region =  os.getenv('jsontemp_Region')
     # environment =  os.getenv('jsontemp_Sub_Environment')
-    mapping_path = os.getenv('mapping_path', 'infra_kit_api/mapping')
+    mapping_path = os.getenv('mapping_path', 'mapping')
     file_name = os.path.join(mapping_path, f"{file_prefix}_{region}_map.json")
     result = read_json_map_file(file_name, environment)
     
@@ -95,6 +104,11 @@ if __name__ == "__main__":
         merged_file = 'merged.json'
         write_to_merged_file(merged_file, merged_data)
         print(f"Merged data written to {merged_file}.")
+        print("Content of merged.json:")
+        with open(merged_file, 'r') as file:
+            print(json.dumps(json.load(file), indent=2))
+        # Trigger the GitHub Action with the merged data
+        
 
         # Trigger the GitHub Action with the merged data
         trigger_github_action(result["Repository_Name"], merged_file)
